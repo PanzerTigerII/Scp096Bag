825cb6b37e3bc7c18ddd3692942f27b7c151b2e0
Panzer VI "Tiger II <bijejajamioszybe@gmail.com>
2025-06-03 16:33:06 +0200

    Add files via upload

---
+++ Scp096Bag/Config.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace Scp096Bag
+{
+	public class Config : IConfig
+	{
+		public bool IsEnabled { get; set; } = true;
+
+		public bool Debug { get; set; } = false;
+
+		[Description("How many seconds does it take to put on a mask on 096")]
+		public int SecondsToUse { get; set; } = 10;
+
+		[Description("Is the mask removed from 096 if he is shot at?")]
+		public bool IsMaskOffByDamage { get; set; } = false;
+	}
+}
